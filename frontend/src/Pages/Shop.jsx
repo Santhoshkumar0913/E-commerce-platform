1d77e8a0a24fcac1f5e58d6fed27e439bf87a951
Santhoshkumar0913 <santhoshkumarp2213@gmail.com>
2026-06-15 23:57:28 +0530

    added NewCollectiosns & newsletter.

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import Newletter from '../Components/NewLetter/NewsLetter'
 
 const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
-      
+      <Offers/>
+      <NewCollections/>
+      <Newletter/>
+     
     </div>
   )
 }
