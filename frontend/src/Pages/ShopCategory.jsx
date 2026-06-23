df46b2d0505a4b171e6821ca5b78e9cc48bc93a7
Santhoshkumar0913 <santhoshkumarp2213@gmail.com>
2026-06-23 23:58:55 +0530

    ShopCategory is updated with png.

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import '../CSS/ShopCategory'
+import { ShopContext } from '../Context/ShopContext'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const {all_product} = useContext(ShopContext)
   return (
-    <div>
+    <div className='shop-category'>
       
     </div>
   )
