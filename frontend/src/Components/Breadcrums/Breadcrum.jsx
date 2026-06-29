882987acd480e6bb8a22ea3c60c1119a4582f6b5
Santhoshkumar0913 <santhoshkumarp2213@gmail.com>
2026-06-29 22:35:09 +0530

    modified the breadcrum files.

--- frontend/src/Components/Breadcrums/Breadcrum.jsx
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import './Breadcrumb.css'
+import './Breadcrum.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
 
-const Breadcrumb = () => {
+const Breadcrum = () => {
     const {product} = props;
   return (
-    <div className='breadcrumb'>
+    <div className='breadcrum'>
       HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
     </div>
   )
 }
 
-export default Breadcrumb
+export default Breadcrum
